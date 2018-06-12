99003d8cc9a768f5d35147f7c431512d9d118246
gustavolbs <gusatvo.santos@ccc.ufcg.edu.br>
2018-06-12 15:15:48 -0300

    "Some modifications on code"

--- git-automatic.sh
+++ git-automatic.sh
@@ -16,10 +16,13 @@ echo
 # This code part will get your credentials and store them.
 git config --global credential.helper cache
 
+# This will set to send all the pushs to the current branch.
+git config --global push.default current
+
 # This will ask you what files you want to send to git.
 echo "Quais arquivos quer adicionar ao git? "
-echo -n "Arquivos que deseja enviar (use " . " para adicionar todos) " 
-read arquivos
+echo -n "Arquivos que deseja enviar (use " . " para adicionar todos): " 
+read -e arquivos
 git add "$arquivos"
 
 # This will ask you the message for the commit.
@@ -34,5 +37,5 @@ git commit -m "$mensagem"
 echo 
 echo "No próximo passo será necessário entrar com seu usuário e senha do git."
 echo "Na próxima vez que executar esse script, não será necessário inserir usuário e senha."  
-git push origin master
+git push
 
