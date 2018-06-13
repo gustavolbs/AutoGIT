2be777522b1c820b8949688479ae25db904027fb
gustavolbs <gustavo.santos@ccc.ufcg.edu.br>
2018-06-13 20:43:25 -0300

    Removed SUDO version

--- bin/install.sh
+++ bin/install.sh
@@ -21,14 +21,14 @@ cd etc
 mkdir bin
 cd
 source /etc/environment
-export PATH=$PATH:/home/$(whoami)/.local/etc/bin
+export PATH=$PATH:/home/$(whoami)/.local/bin
 	echo "- Now, let's configure the execution file..." 
 	{
 		pwd=$(pwd)
 		user=$(whoami)
 
-		cp ~/AutoGIT/noSudo/AutoGit/usr/autogit ~/.local/bin
-		mv ~/AutoGIT/noSudo/AutoGit ~/.local/etc
+		cp ~/AutoGIT/usr/autogit ~/.local/bin
+		mv ~/AutoGIT ~/.local/etc
 
 	} || {
 		echo
