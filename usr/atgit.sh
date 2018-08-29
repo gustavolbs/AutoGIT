94e558b6145ab118d9d259d12bdbcd3b3e796468
gustavolbs <gusatvo.santos@ccc.ufcg.edu.br>
2018-08-29 12:41:16 -0300

    Commit by gustavolbs at quarta 08/29/18 2018 12:41:15

--- usr/atgit.sh
+++ usr/atgit.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-# Without SUDO
 
 echo
 echo " █████╗ ██╗   ██╗████████╗ ██████╗        ██████╗ ██╗████████╗
@@ -16,22 +15,15 @@ git config --global credential.helper store
 # This will set to send all the pushs to the current branch.
 git config --global push.default current
 
-# This will ask you what files you want to send to git.
-echo "What files you want to commit? "
-echo -n "Files (use . to add all): " 
-read -e files
-git add "$files"
+# Add Files
+git add .
 
-# This will ask you the message for the commit.
+# Commit
 echo 
-echo "What message you want to add to your commit? "
-echo -n "Your message: "
-read message
-git commit -m "$message"
+echo "Commiting as: 'Commit by $(whoami) at $(date '+%A %D %Y %X')'"
+git commit -m "Commit by $(whoami) at $(date '+%A %D %Y %X')"
 
 # This will ask you your username and password just one time. After then, it will send the files
 # to the master branch of your repositorie of git without ask your credentials.
 echo 
-git push
-
-
+git push -f
